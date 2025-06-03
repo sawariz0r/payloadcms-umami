0cce71d3e86f388f0a3e38d198aaeb4564a001fc
Oscar Vale <oscar@osuka.dev>
2025-06-03 22:41:42 +0200

    Added files

---
+++ dev/app/(payload)/admin/importMap.js
@@ -0,0 +1,9 @@
+import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'payloadcms-umami/client'
+import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'payloadcms-umami/rsc'
+
+export const importMap = {
+  'payloadcms-umami/client#BeforeDashboardClient':
+    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
+  'payloadcms-umami/rsc#BeforeDashboardServer':
+    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+}
